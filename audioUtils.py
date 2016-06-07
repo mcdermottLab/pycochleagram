5033404ea03a600a89616b312cb46ef9c058faa9
Ray Gonzalez <raysgon@yahoo.com>
2016-06-07 16:37:44 -0400

    Added function to plot waveforms.

--- audioUtils.py
+++ audioUtils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.io import wavfile
 import pyaudio
-
+import matplotlib.pyplot as plt
 
 def parse_rescale_arg(rescale):
   """ Parse the rescaling argument to a standard form. Throws an error if rescale
@@ -37,17 +37,17 @@ def wav_to_array(fn, rescale='standardize'):
         perform rescaling.
 
     Returns:
-      snd, sampFreq (int, np.array): Sampling frequency of the input sound, followed
+      snd, samp_freq (int, np.array): Sampling frequency of the input sound, followed
         by the sound as a numpy-array .
   """
   _rescale = parse_rescale_arg(rescale)
-  sampFreq, snd = wavfile.read(fn)
+  samp_freq, snd = wavfile.read(fn)
   if _rescale == 'standardize':
     snd = snd / float(np.iinfo(snd.dtype).max)  # rescale so max value allowed by precision has value 1
   elif _rescale == 'normalize':
     snd = snd / float(snd.max())  # rescale to [-1, 1]
   # do nothing if rescale is None or ''
-  return snd, sampFreq
+  return snd, samp_freq
 
 
 def play_array(snd_array, pyaudio_params={}):
@@ -72,6 +72,22 @@ def play_array(snd_array, pyaudio_params={}):
   stream = p.open(**_pyaudio_params)
   data = snd_array.astype(np.float32).tostring()
 
-  # stream = p.open(format=pyaudio.paInt16, channels=1, rate=sampFreq, output=True, frames_per_buffer=CHUNKSIZE)
+  # stream = p.open(format=pyaudio.paInt16, channels=1, rate=samp_freq, output=True, frames_per_buffer=CHUNKSIZE)
   # data = snd.astype(snd.dtype).tostring()
   stream.write(data)
+
+
+def plot_waveform(snd_array, samp_freq, n_channels=None):
+  if n_channels is None:
+    n_channels = get_channels(snd_array)
+
+  print n_channels
+  if snd_array.size % n_channels:  # if not 0
+    raise ValueError('Odd amount of data in sound array')
+
+  time_axis = np.arange(0, snd_array.size / n_channels, 1)
+  time_axis = time_axis / float(samp_freq)
+  time_axis = time_axis * 1000  # scale to milliseconds
+  print time_axis[-1]
+  plt.plot(time_axis, snd_array)
+  return time_axis, snd_array
