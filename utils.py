0eb6da089461aed9f78354aee45a13c2d68cf9a6
Ray Gonzalez <raysgon@yahoo.com>
2016-10-10 17:29:44 -0400

    Updated documentation for utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,18 @@ import numpy as np
 
 
 def matlab_arange(start, stop, num):
-  """
-    Mimics MATLAB's sequence generation.
+  """Mimics MATLAB's sequence generation.
 
+  Returns `num + 1` evenly spaced samples, calculated over the interval
+  [`start`, `stop`].
+
+  Args:
+    start (scalar): The starting value of the sequence.
+    stop (scalar): The end value of the sequence.
+    num (int): Number of samples to generate.
+
+  Returns:
+    samples (ndarray): There are `num + 1` equally spaced samples in the closed
+      interval.
   """
   return np.linspace(start, stop, num + 1)
-
-
