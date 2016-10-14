ee333962222d22d1a1efdb1db885b4f287ddff5d
Ray Gonzalez <raysgon@yahoo.com>
2016-10-14 17:09:51 -0400

    fft-type wrappers and custom hilbert transform

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from matplotlib.pyplot import plt as plt
 import numpy as np
 
 
@@ -21,3 +22,230 @@ def matlab_arange(start, stop, num):
       interval.
   """
   return np.linspace(start, stop, num + 1)
+
+
+def cochshow(cochleagram, interact=True):
+  """Helper function to facilitate displaying cochleagrams.
+
+  Args:
+    cochleagram (array): Cochleagram to display with matplotlib.
+    interact (bool, optional): Determines if interactive plot should be shown.
+      If True (default), plot will be shown. If this is False, the figure will
+      be created but not displayed.
+
+  Returns:
+    image : AxesImage
+  """
+  f = plt.imshow(cochleagram, aspect='auto', cmap='viridis')
+  if interact:
+    plt.show()
+  return f
+
+
+def fft(a, mode='auto', params=None):
+  """Provides support for various implementations of the FFT, using numpy's
+  fftpack or pyfftw's fftw.
+
+  Args:
+    a (array): Time-domain signal.
+    mode (str): Determines which FFT implementation will be used. Options are
+      'fftw', 'np', and 'auto'. Using 'auto', will attempt to use a pyfftw
+      implementation with some sensible parameters (if the module is
+      available), and will use numpy's fftpack implementation otherwise.
+    params (dict, None, optional): Dictionary of input arguments to provide to
+      the appropriate fft function. If `mode` is 'auto' and `params` dict is
+      None, sensible values will be chosen. If `params` is not None, it will
+      not be altered.
+
+  Returns:
+    fft_a (array): Signal in the frequency domain in FFT standard order.
+      See numpy.fft() for a description of the output.
+  """
+  # handle 'auto' mode
+  mode, params = _parse_mode(mode, params)
+
+  if mode == 'fftw':
+    import pyfftw
+    return pyfftw.interfaces.numpy_fft.fft(a, **params)
+  elif mode == 'np':
+    return np.fft.fft(a, **params)
+  else:
+    raise NotImplementedError('`fft method is not defined for mode `%s`;' +
+                              'use "auto", "np" or "fftw".')
+
+
+def ifft(a, mode='auto', params=None):
+  """Provides support for various implementations of the IFFT, using numpy's
+  fftpack or pyfftw's fftw.
+
+  Args:
+    a (array): Time-domain signal.
+    mode (str): Determines which IFFT implementation will be used. Options are
+      'fftw', 'np', and 'auto'. Using 'auto', will attempt to use a pyfftw
+      implementation with some sensible parameters (if the module is
+      available), and will use numpy's fftpack implementation otherwise.
+    params (dict, None, optional): Dictionary of input arguments to provide to
+      the appropriate fft function. If `mode` is 'auto' and `params` dict is
+      None, sensible values will be chosen. If `params` is not None, it will
+      not be altered.
+
+  Returns:
+    ifft_a (array): Signal in the time domain. See numpy.ifft() for a
+      description of the output.
+  """
+  # handle 'auto' mode
+  mode, params = _parse_mode(mode, params)
+
+  if mode == 'fftw':
+    import pyfftw
+    return pyfftw.interfaces.numpy_fft.ifft(a, **params)
+  elif mode == 'np':
+    return np.fft.ifft(a, **params)
+  else:
+    raise NotImplementedError('`ifft method is not defined for mode `%s`;' +
+                              'use "np" or "fftw".')
+
+
+def rfft(a, mode='auto', params=None):
+  """Provides support for various implementations of the RFFT, using numpy's
+  fftpack or pyfftw's fftw.
+
+  Args:
+    a (array): Time-domain signal.
+    mode (str): Determines which FFT implementation will be used. Options are
+      'fftw', 'np', and 'auto'. Using 'auto', will attempt to use a pyfftw
+      implementation with some sensible parameters (if the module is
+      available), and will use numpy's fftpack implementation otherwise.
+    params (dict, None, optional): Dictionary of input arguments to provide to
+      the appropriate fft function. If `mode` is 'auto' and `params` dict is
+      None, sensible values will be chosen. If `params` is not None, it will
+      not be altered.
+
+  Returns:
+    rfft_a (array): Signal in the frequency domain in standard order.
+      See numpy.rfft() for a description of the output.
+  """
+  # handle 'auto' mode
+  mode, params = _parse_mode(mode, params)
+
+  if mode == 'fftw':
+    import pyfftw
+    return pyfftw.interfaces.numpy_fft.rfft(a, **params)
+  elif mode == 'np':
+    return np.fft.rfft(a, **params)
+  else:
+    raise NotImplementedError('`rfft method is not defined for mode `%s`;' +
+                              'use "np" or "fftw".')
+
+
+def irfft(a, mode='auto', params=None):
+  """Provides support for various implementations of the IRFFT, using numpy's
+  fftpack or pyfftw's fftw.
+
+  Args:
+    a (array): Time-domain signal.
+    mode (str): Determines which FFT implementation will be used. Options are
+      'fftw', 'np', and 'auto'. Using 'auto', will attempt to use a pyfftw
+      implementation with some sensible parameters (if the module is
+      available), and will use numpy's fftpack implementation otherwise.
+    params (dict, None, optional): Dictionary of input arguments to provide to
+      the appropriate fft function. If `mode` is 'auto' and `params` dict is
+      None, sensible values will be chosen. If `params` is not None, it will
+      not be altered.
+
+  Returns:
+    irfft_a (array): Signal in the time domain. See numpy.irfft() for a
+      description of the output.
+  """
+  # handle 'auto' mode
+  mode, params = _parse_mode(mode, params)
+
+  if mode == 'fftw':
+    import pyfftw
+    return pyfftw.interfaces.numpy_fft.irfft(a, **params)
+  elif mode == 'np':
+    return np.fft.irfft(a, **params)
+  else:
+    raise NotImplementedError('`irfft method is not defined for mode `%s`;' +
+                              'use "np" or "fftw".')
+
+
+def fhilbert(a, axis=None, mode='auto', ifft_params=None):
+  """Compute the Hilbert transform of the provided frequency-space signal.
+
+  This function assumes the input array is already in frequency space, i.e.,
+  it is the output of a numpy-like FFT implementation. This avoids unnecessary
+  repeated computation of the FFT/IFFT.
+
+  Args:
+    a (array): Signal, in frequency space, e.g., a = fft(signal).
+    mode (str): Determines which FFT implementation will be used. Options are
+      'fftw', 'np', and 'auto'. Using 'auto', will attempt to use a pyfftw
+      implementation with some sensible parameters (if the module is
+      available), and will use numpy's fftpack implementation otherwise.
+    params (dict, None, optional): Dictionary of input arguments to provide to
+      the call computing ifft. If `mode` is 'auto' and params dict is None,
+      sensible values will be chosen. If `ifft_params` is not None, it will not
+      be altered.
+
+  Returns:
+    hilbert_a (array): Hilbert transform of input array `a`, in the time domain.
+  """
+  if axis is None:
+    axis = np.argmax(a.shape)
+  N = a.shape[axis]
+  if N <= 0:
+    raise ValueError("N must be positive.")
+
+  # perform the hilbert transform in the frequency domain
+  # algorithm from scipy.signal.hilbert
+  h = np.zeros(N)  # don't modify the input array
+  # create hilbert multiplier
+  if N % 2 == 0:
+    h[0] = h[N // 2] = 1
+    h[1:N // 2] = 2
+  else:
+    h[0] = 1
+    h[1:(N + 1) // 2] = 2
+  ah = a * h  # apply hilbert transform
+
+  return ifft(ah, mode=mode, params=ifft_params)
+
+
+def _parse_mode(mode, params):
+  """Prepare mode and params arguments provided by user for use with
+  utils.fft, utils.ifft, etc.
+
+  Args:
+    mode (str): Determines which FFT implementation will be used. Options are
+      'fftw', 'np', and 'auto'. Using 'auto', will attempt to use a pyfftw
+      implementation with some sensible parameters (if the module is
+      available), and will use numpy's fftpack implementation otherwise.
+    params (dict, None): Dictionary of input arguments to provide to the
+      appropriate fft function. If `mode` is 'auto' and params dict is None,
+      sensible values will be chosen. If `params` is not None, it will not be
+      altered.
+
+  Returns:
+    (tuple): tuple containing:
+
+      out_mode (str): The mode determining the fft implementation to use; either
+        'np' or 'fftw'.
+      out_params (dict): A dictionary containing input arguments to the
+        fft function.
+  """
+  mode == mode.lower()
+  if mode == 'auto':
+    try:
+      import pyfftw
+      mode = 'fftw'
+      if params is None:
+        params = {'planner_effort': 'FFTW_ESTIMATE'}  # FFTW_ESTIMATE seems fast
+    except ImportError:
+      mode = 'np'
+      if params is None:
+        params = {}
+  else:
+    if params is None:
+      params = {}
+  return mode, params
